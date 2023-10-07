081b4590f0f3930042a72c9ac151d98ead2a8770
asiernc <asierncara@gmail.com>
2023-10-07 19:41:13 +0200

    otherMaps OK and doDemo

--- front/src/App.js
+++ front/src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Globe from 'react-globe.gl'
+import axios from 'axios'
 import './App.css';
 import MenuInteraction from './MenuInteraction.js'
 import Library from './Library.js'
@@ -12,7 +13,7 @@ class App extends Component {
       this.state = {
           globeOptions: {
             bumpImageUrl:"moon16.jpg",
-            globeImageUrl: "8k_moon.jpg",
+            globeImageUrl: "moon.jpeg",
             showAtmosphere: true,
             atmosphereColor: 'white',
             atmosphereAltitude: 0.1,
@@ -20,6 +21,7 @@ class App extends Component {
           },
           ui: undefined
       }
+      this.globeRef = React.createRef(); 
 
       this.UIs = [
         <MainMenu
@@ -30,6 +32,7 @@ class App extends Component {
 
         <MenuInteraction
           saveFilteredData={this.saveFilteredData}
+          switchMap={this.switchMap}
         />, 
 
         <Library 
@@ -50,7 +53,46 @@ class App extends Component {
   // Open Github 
   doGit = () => window.open('urlgithub') 
 
-  doDemo = () => {}
+
+  // function to show a short preview of the app
+
+  doDemo = async () => {
+    const animate = async () => {
+      this.globeRef.current.controls().autoRotate = true;
+      this.globeRef.current.controls().autoRotateSpeed = 2;
+      this.globeRef.current.pointOfView({ altitude: 1.75 }, 1500);
+      setTimeout(async () => {
+        this.globeRef.current.controls().autoRotate = false;
+        this.globeRef.current.controls().autoRotateSpeed = 4;
+        this.globeRef.current.controls().autoRotate = true;
+        setTimeout(async () => {
+          this.globeRef.current.controls().autoRotate = false;
+          this.globeRef.current.controls().autoRotateSpeed = 2;
+          this.globeRef.current.controls().autoRotate = true;
+          setTimeout(async() => { 
+            this.globeRef.current.controls().autoRotate = false;
+            this.globeRef.current.pointOfView({ altitude: 2.5 }, 1500);
+            this.insertStartMenu()
+          }, 
+          7500);
+        }, 
+        7500);
+      },
+      7500);
+    }
+
+    await axios.get("http://localhost:8000/get_demo")
+      .then((response) => response.data)
+      .then(async (data) => {
+        const {spacecrafts, moonquakes} = data.demo
+        await this.setState({ui: this.demoMenu})
+        await this.setState({globeOptions: {ringsData: moonquakes, pointsData: spacecrafts}})
+        await animate()
+      })
+      .catch((error) => {
+        console.log(error);
+    }); 
+  }
 
 
   // function to save the data from the filter
@@ -70,15 +112,26 @@ class App extends Component {
     })
   }
 
+  // function to switch the globe map
+  switchMap = async (value) => {
+      await this.setState({
+          globeOptions: {
+            globeImageUrl: value + '.jpeg',
+          }
+        })
+  }
+
+
   render () {
     const { ui, globeOptions } = this.state
+    const globeRef = this.globeRef
     return (
       <>
         <div className="Screen">
           {ui}
         </div>
         <div className='Globe'>
-          <Globe {...globeOptions}/>
+          <Globe ref={globeRef} {...globeOptions}/>
         </div>
       </>
     );
@@ -86,9 +139,3 @@ class App extends Component {
 }
 
 export default App;
-
-
-
-
-
-/* componentDidMount = () => this.setState({ui : this.mainMenu}) */
