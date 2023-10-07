4ad35702c4e510ddf08a20fa826788338b9454c7
asiernc <asierncara@gmail.com>
2023-10-07 16:16:26 +0200

    fix git ignore

--- front/src/App.js
+++ front/src/App.js
@@ -1,20 +1,96 @@
 import React, { Component } from 'react'
 import Globe from 'react-globe.gl'
 import './App.css';
+import MenuInteraction from './MenuInteraction.js'
+import Library from './Library.js'
+import MainMenu from './MainMenu.js'
+
+
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+      this.state = {
+          globeOptions: {
+            bumpImageUrl:"moon16.jpg",
+            globeImageUrl: "8k_moon.jpg",
+            showAtmosphere: true,
+            atmosphereColor: 'white',
+            atmosphereAltitude: 0.1,
+            backgroundImageUrl: '8k_stars_milky_way.jpg',
+          },
+          ui: undefined
+      }
+
+      this.UIs = [
+        <MainMenu
+          changeUI={this.changeUI}
+          />, 
+
+          <></>,
+
+        <MenuInteraction
+          setFilteredData={this.saveFilteredData} 
+        />, 
+
+        <Library 
+        
+        />, 
+      
+      ]
+  }
+  componentDidMount = () => this.setState({ui : this.UIs[0]})
+  
+  // change for switch function, 
+  changeUI = (index) => {
+    if(index===1)
+      {this.doDemo()};
+
+    this.setState({ui: this.UIs[index]})}
+  
+  // Open Github 
+  doGit = () => window.open('urlgithub') 
+
+  doDemo = () => {}
 
 
+  // function to save the data from the filter
+  saveFilteredData = (pointsData, pointLabel, ringsData) => {
+    this.setState({
+      globeOptions: {
+        pointsData: pointsData,
+        pointLabel: pointLabel,
+        ringsData: ringsData,
+        
+        ringMaxRadius: 'maxR',
+        ringPropagationSpeed: 'propSpeed',
+        ringRepeatPeriod: 'repeatPeriod',
+        ringColor: 'ringColor',
+        ringLabel: 'ringLabel',
+      }
+    })
+  }
 
   render () {
-  return (
-    <>
-    <div className="root"
-
-    </div>
-    </>
-  );
+    const { ui, globeOptions } = this.state
+    return (
+      <>
+        <div className="Screen">
+          {ui}
+        </div>
+        <div className='Globe'>
+          <Globe {...globeOptions}/>
+        </div>
+      </>
+    );
 }
 }
 
 export default App;
+
+
+
+
+
+/* componentDidMount = () => this.setState({ui : this.mainMenu}) */
---
+++ front/src/Moonquakes.js
@@ -0,0 +1,82 @@
+import { Component } from "react";
+
+class Moonquakes extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showSlider: false,
+            year: 1971,
+            magnitude: 0,
+        }
+        
+    }
+    // function to extends the slider
+    toggleSlider = () => { 
+        this.setState({showSlider: !this.state.showSlider}) 
+    }
+
+    render() { 
+        const {year, allYears, magnitude, allMagnitudes} = this.state
+        return ( 
+            <>
+                <div className="slider">
+                    <button onClick={this.toggleSlider}>Moonquakes</button>
+                </div>
+                {this.state.showSlider &&  (
+                    <div className="control-container">
+                        <div className="range-buttons">
+                            <label htmlFor="yearRange">Year selected: {year} </label>
+                            <input 
+                                type="range" 
+                                min="1971" 
+                                max="1976" 
+                                step="1"
+                                value={year}
+                                disabled={allYears}
+                                onChange={(e) => this.setState({year: e.target.value})}
+                            />
+                            <label>
+                                <input 
+                                    type="checkbox" 
+                                    checked={allYears} 
+                                    onChange={(e) => this.setState({allYears: e.target.checked})}
+                                />
+                                All
+                            </label>
+                        </div>
+
+                        <div className="range-buttons">
+                            <label htmlFor="magnRange">Magn selected: {magnitude}</label>
+                            <input 
+                                type="range" 
+                                min="1971" 
+                                max="1976" 
+                                step="1"
+                                value={magnitude} 
+                                disabled={allMagnitudes}
+                                onChange={(e) => this.setState({year: e.target.value})}
+                            />
+                            <label>
+                                <input 
+                                    type="checkbox" 
+                                    checked={allMagnitudes} 
+                                    onChange={(e) => this.setState({allMagnitudes: e.target.checked})}
+                                />
+                                All
+                            </label>
+                        </div>
+                        <div className="control-buttons">
+                            <button onClick={() => {this.props.setFilterMoonquake(year, allYears, magnitude, allMagnitudes)}}>Filter</button>
+                            <button onClick={() => {this.props.setFilterMoonquake({year: 0})}}>Clear</button>
+                        </div>
+                    </div>
+
+
+                )}  
+            
+            </>
+        );
+    }
+}
+
+export default Moonquakes;
